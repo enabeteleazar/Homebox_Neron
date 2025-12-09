d7a2bac6a1b372e5fe0a4c1ea3a1882f1aab66cc
enabeteleazar <nabet.eleazar@gmail.com>
2025-12-09 23:30:58 +0100

    fix: correction cnx neron-telegram.

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/eleazar/homebox/venv
+set -gx VIRTUAL_ENV /home/eleazar/Homebox_Neron/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
